0817f147408680cdb4df83aa3f01fb3f26e7a0bb
Yutong Chen <yc3993@columbia.edu>
2022-03-27 15:24:22 -0400

    commit

---
+++ front/script.js
@@ -0,0 +1,30 @@
+const image_input = document.querySelector("#image_input");
+const label_input = document.querySelector("#fname");
+var labels = document.getElementById("fname").value;  
+
+
+
+image_input.addEventListener("change", function(){
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+        uploaded_image = reader.result;
+        document.querySelector("#display_image").style.backgroundImage = `url(${uploaded_image})`
+    });
+    reader.readAsDataURL(this.files[0]);
+    var labels = document.getElementById("fname").value;  
+    console.log(labels);
+    var x = this.files[0]['name']
+    fetch(' https://dl2y7ow6n3.execute-api.us-east-1.amazonaws.com/S1/upload/b2photostorage/'+x,{
+        method:'PUT',
+        headers:{
+            'Content-Type': this.files[0].type,
+            "x-amz-meta-customLabels": labels        
+        },
+        body: this.files[0]
+    }) 
+    
+})
+
+
+
+
---
+++ lambda/index-photos.py
@@ -0,0 +1,72 @@
+from requests_aws4auth import AWS4Auth
+import json
+import urllib.parse
+import boto3
+import requests
+
+
+s3 = boto3.client('s3')
+client=boto3.client('rekognition')
+
+
+def lambda_handler(event, context):
+    # print("Received event: " + json.dumps(event, indent=2))
+
+    # Get the object from the event and show its content type
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    print(bucket)
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    print(key)
+    labels=[]
+    try:
+        response = s3.get_object(Bucket=bucket, Key=key)
+        temp = s3.head_object(Bucket=bucket, Key=key)
+        try:
+            for i in temp['Metadata']['customlabels'].split(','):
+                labels.append(i)
+        except:
+            labels=[]
+        #print(temp)
+        print('1')
+        print(labels)
+        predict = client.detect_labels(Image={'S3Object':{'Bucket':bucket,'Name':key}}, MaxLabels=4, MinConfidence=50)
+        print('PREDICT',predict)
+        for label in predict['Labels']:
+            labels.append(label['Name'])
+        #print(1)
+        print(labels)
+        res = {
+            'objectKey': key,
+            'bucket': bucket,
+            'createdTimestamp': temp['ResponseMetadata']['HTTPHeaders']['date'],
+            'labels': labels
+        }
+        print(res)
+
+        # post to es
+        region = 'us-east-1'
+        service = 'es'
+        credentials = boto3.Session().get_credentials()
+        awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+        host = 'https://search-photosearch-keg5p47cv2pwbkvyvalwgnv7lq.us-east-1.es.amazonaws.com'
+        index = 'photo'
+        type = 'lambda-type'
+        url = host + "/" + index + "/" + type
+        headers = { "Content-Type": "application/json" }
+        r = requests.post(url, auth=awsauth, json=res, headers=headers)
+        print(r.text)
+        res = {
+        "isBase64Encoded": False,
+        "statusCode": 200,
+        "headers": {'Access-Control-Allow-Origin': '*'},
+        "body": "success"
+        }
+        return res
+    except Exception as e:
+        res = {
+        "isBase64Encoded": False,
+        "statusCode": 400,
+        "headers": {'Access-Control-Allow-Origin': '*'},
+        "body": "failed"
+        }
+        return res
